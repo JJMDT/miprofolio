f4de344f3d540ea440f22291e338de8db8faf217
JJMDT <127325507+JJMDT@users.noreply.github.com>
2024-11-26 18:09:00 -0300

    se agregaron proyectos

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,57 +1,58 @@
 import React from "react";
 import { ListProyect } from "./ListProyect";
 import { Accordion } from "react-bootstrap";
+import { proyectosData } from "../proyectosData";
 
-const proyectosData = [
-  {
-    title: "LandingPage",
-    subtitle: "Porfolio Personal",
-    description: "Porfolio personal con toda la información",
-    imageUrl: "/assets/proyecto/porfolio.JPG",
-    link: "https://soyjuan.netlify.app//",
-    type: "web"
-  },
-  {
-    title: "Pokemon",
-    subtitle: "Pokemon's cards",
-    description: "Encuentra a tu pokemon favorito en esta aplicación",
-    imageUrl: "/assets/proyecto/poke.avif",
-    link: "https://glistening-horse-b219d1.netlify.app/",
-    type: "web"
-  },
-  {
-    title: "Rick and Morty",
-    subtitle: "Rick and Morty cards",
-    description: "Encuentra a tu personajo favorito en esta aplicación",
-    imageUrl: "/assets/proyecto/rick.avif",
-    link: "https://animated-travesseiro-528f40.netlify.app/",
-    type: "web"
-  },
-  {
-    title: "Tienda Funko",
-    subtitle: "Tienda web",
-    description: "Tienda online de figuras coleccionables FUNKOPOP!",
-    imageUrl: "/assets/proyecto/pop.jpg",
-    link: "https://tiendafunko.netlify.app/",
-    type: "ecommerce"
-  },
-  {
-    title: "Weather Channel",
-    subtitle: "Consulta el clima",
-    description: "¿Quieres saber cómo va a estar el clima en la ciudad?",
-    imageUrl: "/assets/proyecto/clima.png",
-    link: "https://appclimaapp.netlify.app/",
-    type: "app"
-  },
-  {
-    title: "Color Game",
-    subtitle: "Adivina el color",
-    description: "Juego donde tendrás que adivinar un color al azar",
-    imageUrl: "/assets/proyecto/color.png",
-    link: "https://color-game-pledu.netlify.app/",
-    type: "app"
-  }
-];
+// const proyectosData = [
+//   {
+//     title: "LandingPage",
+//     subtitle: "Porfolio Personal",
+//     description: "Porfolio personal con toda la información",
+//     imageUrl: "/assets/proyecto/porfolio.JPG",
+//     link: "https://soyjuan.netlify.app//",
+//     type: "web"
+//   },
+//   {
+//     title: "Pokemon",
+//     subtitle: "Pokemon's cards",
+//     description: "Encuentra a tu pokemon favorito en esta aplicación",
+//     imageUrl: "/assets/proyecto/poke.avif",
+//     link: "https://glistening-horse-b219d1.netlify.app/",
+//     type: "web"
+//   },
+//   {
+//     title: "Rick and Morty",
+//     subtitle: "Rick and Morty cards",
+//     description: "Encuentra a tu personajo favorito en esta aplicación",
+//     imageUrl: "/assets/proyecto/rick.avif",
+//     link: "https://animated-travesseiro-528f40.netlify.app/",
+//     type: "web"
+//   },
+//   {
+//     title: "Tienda Funko",
+//     subtitle: "Tienda web",
+//     description: "Tienda online de figuras coleccionables FUNKOPOP!",
+//     imageUrl: "/assets/proyecto/pop.jpg",
+//     link: "https://tiendafunko.netlify.app/",
+//     type: "ecommerce"
+//   },
+//   {
+//     title: "Weather Channel",
+//     subtitle: "Consulta el clima",
+//     description: "¿Quieres saber cómo va a estar el clima en la ciudad?",
+//     imageUrl: "/assets/proyecto/clima.png",
+//     link: "https://appclimaapp.netlify.app/",
+//     type: "app"
+//   },
+//   {
+//     title: "Color Game",
+//     subtitle: "Adivina el color",
+//     description: "Juego donde tendrás que adivinar un color al azar",
+//     imageUrl: "/assets/proyecto/color.png",
+//     link: "https://color-game-pledu.netlify.app/",
+//     type: "app"
+//   }
+// ];
 
 
 const Title = ({ text }) => {
--- src/proyectosData.js
+++ src/proyectosData.js
@@ -1,44 +1,52 @@
-const proyectosData = [
-    {
-      title: "LandingPage",
-      subtitle: "Porfolio Personal",
-      description: "Porfolio personal con toda la información",
-      imageUrl: "/assets/proyecto/porfolio.JPG",
-      link: "https://color-game-pledu.netlify.app/",
-      type: "web"
-    },
-    {
-      title: "Pokemon",
-      subtitle: "Pokemon's cards",
-      description: "Encuentra a tu pokemon favorito en esta aplicación",
-      imageUrl: "/assets/proyecto/poke.avif",
-      link: "https://color-game-pledu.netlify.app/",
-      type: "web"
-    },
-    {
-      title: "Tienda Funko",
-      subtitle: "Tienda web",
-      description: "Tienda online de figuras coleccionables FUNKOPOP!",
-      imageUrl: "/assets/proyecto/pop.jpg",
-      link: "https://tiendafunko.netlify.app/",
-      type: "ecommerce"
-    },
-    {
-      title: "Weather Channel",
-      subtitle: "Consulta el clima",
-      description: "¿Quieres saber cómo va a estar el clima en la ciudad?",
-      imageUrl: "/assets/proyecto/clima.png",
-      link: "https://appclimaapp.netlify.app/",
-      type: "app"
-    },
-    {
-      title: "Color Game",
-      subtitle: "Adivina el color",
-      description: "Juego donde tendrás que adivinar un color al azar",
-      imageUrl: "/assets/proyecto/color.png",
-      link: "https://color-game-pledu.netlify.app/",
-      type: "app"
-    },
+export const proyectosData = [
+  {
+        title: "LandingPage",
+        subtitle: "Porfolio Personal",
+        description: "Porfolio personal con toda la información",
+        imageUrl: "/assets/proyecto/porfolio.JPG",
+        link: "https://soyjuan.netlify.app//",
+        type: "web"
+      },
+      {
+        title: "Pokemon",
+        subtitle: "Pokemon's cards",
+        description: "Encuentra a tu pokemon favorito en esta aplicación",
+        imageUrl: "/assets/proyecto/poke.avif",
+        link: "https://glistening-horse-b219d1.netlify.app/",
+        type: "web"
+      },
+      {
+        title: "Rick and Morty",
+        subtitle: "Rick and Morty cards",
+        description: "Encuentra a tu personajo favorito en esta aplicación",
+        imageUrl: "/assets/proyecto/rick.avif",
+        link: "https://animated-travesseiro-528f40.netlify.app/",
+        type: "web"
+      },
+      {
+        title: "Tienda Funko",
+        subtitle: "Tienda web",
+        description: "Tienda online de figuras coleccionables FUNKOPOP!",
+        imageUrl: "/assets/proyecto/pop.jpg",
+        link: "https://tiendafunko.netlify.app/",
+        type: "ecommerce"
+      },
+      {
+        title: "Weather Channel",
+        subtitle: "Consulta el clima",
+        description: "¿Quieres saber cómo va a estar el clima en la ciudad?",
+        imageUrl: "/assets/proyecto/clima.png",
+        link: "https://appclimaapp.netlify.app/",
+        type: "app"
+      },
+      {
+        title: "Color Game",
+        subtitle: "Adivina el color",
+        description: "Juego donde tendrás que adivinar un color al azar",
+        imageUrl: "/assets/proyecto/color.png",
+        link: "https://color-game-pledu.netlify.app/",
+        type: "app"
+      },
     {
       title: "Patitas al Rescate",
       subtitle: "Encuentra a tu mascota perdida",

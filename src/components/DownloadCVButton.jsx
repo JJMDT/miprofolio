c55093be150035cc3d99a8db7630282cd30a9158
JJMDT <127325507+JJMDT@users.noreply.github.com>
2025-01-27 18:48:07 -0300

    actualizacion de cv

--- src/components/DownloadCVButton.jsx
+++ src/components/DownloadCVButton.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
 export const DownloadCVButton = () => {
- // const urlCV = `https://drive.google.com/file/d/159CLtclIqmk7u7e0nfk17ZeqOKiCsHMe/view?usp=drive_link`;
-  // const urlCV = `https://drive.google.com/file/d/1gJk1tVQrGSS5cmCUmDO9TmC_iE1TyVFs/view?usp=drive_link`;
-  const urlCV = `https://drive.google.com/file/d/1m3tWdGbGFIr1336Ln9bst1fQPceo-aa8/view?usp=drivesdk`;
+  //const urlCV = `https://drive.google.com/file/d/1m3tWdGbGFIr1336Ln9bst1fQPceo-aa8/view?usp=drivesdk`;
+  const urlCV = `https://drive.google.com/file/d/1JQ7yvw0aBDvXo89cslBaXHnIpcPcFceq/view?usp=drive_link`;
   return (
       <a
         href={urlCV}

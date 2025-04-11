6cfe8be527129b36d3573cad29dda09f10d1e562
JJMDT <127325507+JJMDT@users.noreply.github.com>
2025-04-11 10:51:58 -0300

    Update DownloadCVButton.jsx
    change cv

--- src/components/DownloadCVButton.jsx
+++ src/components/DownloadCVButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const DownloadCVButton = () => {
   //const urlCV = `https://drive.google.com/file/d/1m3tWdGbGFIr1336Ln9bst1fQPceo-aa8/view?usp=drivesdk`;
-  const urlCV = `https://drive.google.com/file/d/1JQ7yvw0aBDvXo89cslBaXHnIpcPcFceq/view?usp=drive_link`;
+  const urlCV = `https://drive.google.com/file/d/1k6jshQQ4JwkGmTS7bTd-ytu7ydl7cPJn/view?usp=drive_link`;
   return (
       <a
         href={urlCV}
